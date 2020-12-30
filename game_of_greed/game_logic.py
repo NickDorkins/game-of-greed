1514f8b964cbae102beb622aa51e296f848567ea
NickDorkins <nick.dorkins@gmail.com>
2020-12-29 21:25:33 -0800

    initial build

---
+++ game_of_greed/game_logic.py
@@ -0,0 +1,15 @@
+
+from collections import Counter
+
+class GameLogic:
+    pass
+
+#  Calculate score (Static method)
+#  Inputs - Tuple
+#  Outputs - Integer (Score Value)
+
+
+#  Roll Dice (Static Method)
+#  Input - Integer (1 -6)
+#  Output - Tuple (List of face value per dice)
+
