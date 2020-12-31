70697a64ab49871d97bf0c02f3247ac66c029e8d
Choi702 <jaeychoi1@gmail.com>
2020-12-30 21:05:28 -0800

    bank and dice roll test passing

--- game_of_greed/game_logic.py
+++ game_of_greed/game_logic.py
@@ -1,10 +1,39 @@
 
 from collections import Counter
+import random
+
 
 class GameLogic:
-    pass
+    
+  
 
 #  Calculate score (Static method)
+
+    @staticmethod
+    def roll_dice(num_die):
+        diceroll = []
+        for die in range(1, num_die + 1):
+            add_score = random.randint(1, 6) 
+            diceroll.append(add_score)
+            # print(add_score)
+    
+        return tuple(diceroll)
+
+
+
+
+
+
+
+test = GameLogic()
+print(test.roll_dice(6))
+ 
+
+
+
+        
+        
+ 
 #  Inputs - Tuple
 #  Outputs - Integer (Score Value)
 
